1817228c12310ec46cd72afbcb30707464c7b33f
Aspen James <aspenjames@tqca.org>
2018-01-29 20:21:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,10 +7,10 @@ function init() {
  function onKeyDownHandler(e){
    const key = parseInt(e.which);
    
-   if (key === alphabet[index]) {
+   if (key === code[index]) {
      index++;
      
-     if (index === alphabet.length){
+     if (index === code.length){
        alert("Konami activated");
        index = 0;
      }
