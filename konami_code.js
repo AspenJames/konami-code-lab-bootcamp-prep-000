99ecb982b9a73afefce2d3784c073dab05fedc7f
Aspen James <aspenjames@tqca.org>
2018-01-29 20:25:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,7 +7,7 @@ function init() {
   let index = 0;
   
  function onKeyDownHandler(e){
-   const key = parseInt(e.which);
+   const key = parseInt(e.detail || e.which);
    
    if (key === code[index]) {
      index++;
